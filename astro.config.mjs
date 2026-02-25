8474858a2f6c06081c32a9919e399c390b1e0a6e
itouhiroki <hrking815@gmail.com>
2026-02-25 13:38:19 +0900

    調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config"
 const isProd = process.env.NODE_ENV === "production"
 
 export default defineConfig({
-  site: "https://hiroking815.github.io",
+  site: "https://pirokinchos.github.io",
   base: isProd ? "/oshi/" : "/",
 })
